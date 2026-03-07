195a97ed30a77054dfb7a52d5d2a94279f0a8009
Your Name <tarang5757@gmail.com>
2026-03-07 12:52:26 -0500

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import HeroSection from "@/components/HeroSection";
+import BookCard from "@/components/BookCard";
+import {sampleBooks} from "@/lib/constants";
+
+const Page = async ({ searchParams }: { searchParams: Promise<{ query?: string }> }) => {
+
+
+    return (
+        <main className="wrapper container">
+            <HeroSection />
+
+            <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
+                <h2 className="text-3xl font-serif font-bold text-[#212a3b]">Recent Books</h2>
+
+            </div>
+
+            <div className="library-books-grid">
+                {sampleBooks.map((book) => (
+                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+                ))}
+            </div>
+        </main>
+    )
+}
+
+export default Page
